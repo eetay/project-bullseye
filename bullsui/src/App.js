02ded907b28f25f88ebb15cf21ac25e4bbb05091
ENatan <Eetay.Natan@harman.com>
2019-04-07 04:56:02 -0700

    python server push location of target

--- bullsui/src/App.js
+++ bullsui/src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './bullseye.png'
 import './App.css';
+
+
 
 class App extends Component {
   constructor (props) {
     super()
     this.imageRef = React.createRef();
+    this.x=this.y=1
   }
 
   componentDidMount(_props) {
     var self = this
+    self.exampleSocket = new WebSocket("ws://localhost:9999");
+    self.exampleSocket.onmessage = function (event) {
+      console.log('got event', event.data);
+      let {offsetTop, offsetLeft} = JSON.parse(event.data)
+      self.moveTarget({
+        image: self.imageRef.current, 
+        offsetTop, 
+        offsetLeft
+      })
+    }
+    self.exampleSocket.onopen = function (event) {
+      console.log('connected', event);
+      if (event.type === 'open') {
+        self.exampleSocket.connected = true 
+      }
+    };
+    /*
     let a = setInterval(function () {
-      let offsetTop = Math.floor(Math.random() * 10) * 10;
-      let offsetLeft = Math.floor(Math.random() * 10) * 10;
+      let offsetTop = self.y++;
+      let offsetLeft = self.x++;
       self.moveTarget({image: self.imageRef.current, offsetTop, offsetLeft})
     }, 500)
     console.log(a)
+    */
   }
 
   moveTarget = ({image, offsetTop, offsetLeft}) => {
@@ -25,14 +46,14 @@ class App extends Component {
     img.style.top = offsetTop + 'px';
     img.style.left = offsetLeft + 'px';
     img.style.display = "block";
+    if (this.exampleSocket.connected) this.exampleSocket.send(JSON.stringify({offsetTop, offsetLeft}));
   }
 
   render() {
-    //let ctx = canvas.getContext('2d');
     return (
       <div className="App">
-        <header className="App-header" height="400px" width="400px">
-          <img ref={this.imageRef} src={logo} className="App-logo" alt="logo" /> 
+        <header className="App-header" height="100%" width="100%">
+          <img ref={this.imageRef} src={logo} className="App-logo" alt="logo"/> 
         </header>
       </div>
     );
