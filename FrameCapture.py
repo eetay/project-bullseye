8c24a642fa1e609bb81a842d5ce96d99c23b602d
tomera <tomer.ayun@harman.com>
2019-04-07 12:13:23 +0300

    First Draft

---
+++ FrameCapture.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    img_hsv=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,255,255])
+    mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+    lower_red = np.array([170,50,50])
+    upper_red = np.array([180,255,255])
+    mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+    # join my masks
+    mask = mask0+mask1
+
+    # set my output img to zero everywhere except my mask
+    output_img = frame.copy()
+    output_img[np.where(mask==0)] = 255
+
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame', output_img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
